daefc8123b8b8612b4835d5878863e396f2d188a
vitorAllux <vitor@eloverde.com.br>
2025-06-02 19:30:27 -0300

    feat: add environment

---
+++ environment.example.ts
@@ -0,0 +1,8 @@
+export const ENV = {
+  API_URL: 'http://<SEU-IP>:8000/api', // backend Laravel
+  DB_HOST: 'localhost',
+  DB_PORT: '3306',
+  DB_USER: 'root',
+  DB_PASSWORD: '',
+  DB_NAME: 'cattle_id',
+};
--- environment.ts
+++ environment.ts
@@ -1,18 +1,9 @@
 export const ENV = {
-    API_URL: 'https://cattle-id-platform-production.up.railway.app/api',
-    DB_HOST: 'centerbeam.proxy.rlwy.net',
-    DB_PORT: '18935',
+    API_URL: 'http://192.168.0.119:8000/api',
+    DB_HOST: 'localhost',
+    DB_PORT: '3306',
     DB_USER: 'root',
-    DB_PASSWORD: 'SrxpqAFwJklucvBmVbjSHcdfCTXUCYWD',
-    DB_NAME:'railway',
+    DB_PASSWORD: '',
+    DB_NAME: 'cattle_id',
 };
-
-// export const ENV = {
-//     API_URL: 'http://192.168.0.119:8000/api',
-//     DB_HOST: 'localhost',
-//     DB_PORT: '3306',
-//     DB_USER: 'root',
-//     DB_PASSWORD: '',
-//     DB_NAME: 'cattle_id',
-// };
 
